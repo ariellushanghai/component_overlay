c27584cbd270ae3799f47e623d16c839be6a7c89
陆欢 <luhuan@fosun.com>
2019-08-12 16:07:22 +0800

    1st commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import 'babel-polyfill'
+import Overlay from './overlay';
+
+const init = _ => {
+    const confirmCallback = _ => alert(JSON.stringify(`confirmCallback() @ ${new Date()}`));
+    const cancelCallback = _ => alert(JSON.stringify(`cancelCallback() @ ${new Date()}`));
+
+
+    const demo_callback = new Overlay({
+        title: '回调示例',
+        content: `<p>点 'Confirm()' 执行 "confirmCallback"</p>
+                  <p>点 'Cancel()' 执行 "cancelCallback"</p>
+                 `,
+        callback_confirm: confirmCallback, // Function
+        callback_cancel: cancelCallback, // Function
+        text_confirm: 'Confirm()',
+        text_cancel: 'Cancel()',
+        flag_html_as_content: true,
+    });
+
+    const demo_ricktext = new Overlay({
+        title: '标题文字',
+        content: `<h2>富文本内容</h2>
+                  <p style="color: red;">红字</p>
+                  <p style="text-decoration: line-through">划线</p>
+                  <p style="background-color: greenyellow">"background-color: greenyellow"</p>
+                 `,
+        flag_html_as_content: true
+    });
+
+    const demo_control = new Overlay({
+        title: '编程控制',
+        content: `<span>延时1秒后调用实例方法:</span>
+                  <pre style="display: inline-block;">close()</pre>
+                  <span>关闭</span>
+                 `,
+        flag_html_as_content: true
+    });
+
+    document.querySelector('#btn_0').addEventListener('click', () => {
+        demo_callback.open()
+    });
+    document.querySelector('#btn_1').addEventListener('click', () => {
+        demo_ricktext.open()
+    });
+    document.querySelector('#btn_2').addEventListener('click', () => {
+        demo_control.open();
+        setTimeout(_ => demo_control.close(), 1e3)
+    });
+
+};
+
+window.addEventListener('DOMContentLoaded', init);
+
---
+++ src/overlay/index.js
@@ -0,0 +1,131 @@
+// @flow
+import styles from './index.styl';
+
+const DEFAULT_OPTIONS = {
+    title: '', // String
+    content: '', // String
+    callback_confirm: null, // Function
+    callback_cancel: null, // Function
+    text_confirm: '确定', // String
+    text_cancel: '取消', // String
+    flag_html_as_content: false, // Boolean
+    flag_lock_page_scroll: true // Boolean
+};
+
+const VALID_OPTION_KEYS = Object.keys(DEFAULT_OPTIONS);
+
+class Overlay {
+    constructor(input = {}) {
+        let accepted = {};
+        VALID_OPTION_KEYS.forEach(k => {
+            if (k in input) {
+                // TODO: type check
+                accepted[k] = input[k]
+            }
+        });
+        this._options = Object.assign({}, DEFAULT_OPTIONS, accepted);
+        this._id = `${(new Date()).valueOf()}${Math.ceil(Math.random() * 1e3)}`;
+        this._isMounted = false; // 挂载到DOM
+        this._isOpened = false; // 显示<Overlay/>
+        this._mountNode = null; // 挂载DOM Node
+        //
+        this.init()
+    }
+
+    // init
+    init() {
+        const $elem = this.createElems();
+        this.mount($elem);
+        this.bindEvents();
+    }
+
+    // create <overlay/> dom structure with _options
+    createElems() {
+        const fragment = document.createDocumentFragment();
+        // 覆盖层容器
+        const $cover = document.createElement('div');
+        $cover.setAttribute('id', `overlay_instance_${this._id}`);
+        $cover.setAttribute('class', `overlay-instance ${styles.mask} ${styles.closed}`);
+        // 模态窗
+        const $modal = document.createElement('div');
+        $modal.setAttribute('class', `${styles.modal}`);
+        // 模态窗的标题
+        const $title = document.createElement('div');
+        $title.setAttribute('class', `${styles.title}`);
+        $title.innerText = String(this._options.title);
+        // 模态窗的内容
+        const $content = document.createElement('div');
+        $content.setAttribute('class', `${styles.content}`);
+        if (this._options.flag_html_as_content) {
+            $content.innerHTML = String(this._options.content)
+        } else {
+            $content.innerText = String(this._options.content)
+        }
+        // 模态窗的按钮组
+        const html_str = `<button class="btn cancel">${this._options.text_cancel}</button>
+                          <button class="btn confirm primary">${this._options.text_confirm}</button>
+                         `;
+        const $buttons = document.createElement('div');
+        $buttons.setAttribute('class', `${styles.buttonContainer}`);
+        $buttons.innerHTML = html_str;
+        // 生成子树
+        $modal.appendChild($title);
+        $modal.appendChild($content);
+        $modal.appendChild($buttons);
+        $cover.appendChild($modal);
+        //
+        fragment.appendChild($cover);
+        return fragment
+    }
+
+    // mount an <overlay/> instance to body
+    mount(fragment) {
+        if (!this._isMounted) {
+            // const fragment = document.createDocumentFragment();
+            // fragment.appendChild(_);
+            document.body.appendChild(fragment);
+            this._isMounted = true;
+            this._mountNode = document.querySelector(`#overlay_instance_${this._id}`)
+        }
+    }
+
+    // 响应组件内部按钮click
+    bindEvents() {
+        if (this._mountNode) {
+            ['confirm', 'cancel'].forEach(__ => {
+                document.querySelector(`#overlay_instance_${this._id} .btn.${__}`).addEventListener('click', () => {
+                    this.close();
+                    if (typeof this._options[`callback_${__}`] === 'function') {
+                        return this._options[`callback_${__}`]()
+                    }
+                });
+            });
+        }
+    }
+
+    // reveal overlay
+    open() {
+        console.log('open');
+        if (this._isMounted) {
+            this._mountNode.classList.remove(`${styles.closed}`);
+            if (this._options.flag_lock_page_scroll) {
+                // lock body scrolling
+                document.body.classList.add(`${styles.noOverflow}`)
+            }
+        }
+    }
+
+    // close overlay
+    close() {
+        console.log('close');
+        if (this._isMounted) {
+            this._mountNode.classList.add(`${styles.closed}`);
+            if (this._options.flag_lock_page_scroll) {
+                // unlock body scrolling
+                document.body.classList.remove(`${styles.noOverflow}`)
+            }
+        }
+    }
+}
+
+export default Overlay
